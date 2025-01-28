a8bc5a0618f4c147b8c30db004f55c706398c9e5
Muhammad Abdullah <ma2404374@gmail.com>
2025-01-28 18:46:15 +0500

    starting a medical chatbot project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Medical Chatbot",
+    version="0.0.0",
+    author="Muhammad Abdullah",
+    author_email="aideveloper453@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
