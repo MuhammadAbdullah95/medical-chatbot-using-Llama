3970922e7ff196264a71686bea3c3468d7d3ebc9
Muhammad Abdullah <ma2404374@gmail.com>
2025-02-04 22:30:47 +0500

    almost complete

---
+++ app.py
@@ -0,0 +1,86 @@
+from flask import Flask, render_template, jsonify, request
+from src.helper import download_hugging_face_embeddings
+from pinecone import Pinecone, ServerlessSpec
+from langchain_groq import ChatGroq
+from langchain_pinecone import PineconeVectorStore
+from langchain.prompts import PromptTemplate
+from langchain_community.llms import CTransformers
+from langchain.chains import RetrievalQA
+from dotenv import load_dotenv
+import time
+from src.prompt import *
+import os
+
+app = Flask(__name__)
+
+load_dotenv()
+
+PINECONE_API_KEY = os.environ.get("PINECONE_API_KEY")
+GROQ_API_KEY = os.environ.get("GROQ_API_KEY")
+
+embeddings = download_hugging_face_embeddings()
+
+# Initializing the Pinecone
+pc = Pinecone(api_key=PINECONE_API_KEY)
+
+index_name = "medical-chatbot"  # change if desired
+
+existing_indexes = [index_info["name"] for index_info in pc.list_indexes()]
+
+if index_name not in existing_indexes:
+    pc.create_index(
+        name=index_name,
+        dimension=384,
+        metric="cosine",
+        spec=ServerlessSpec(cloud="aws", region="us-east-1"),
+    )
+    while not pc.describe_index(index_name).status["ready"]:
+        time.sleep(1)
+
+index = pc.Index(index_name)
+
+index_name = "medical-bot"
+
+docsearch = PineconeVectorStore(index=index, embedding=embeddings)
+
+# Loading the index
+
+
+PROMPT = PromptTemplate(
+    template=prompt_template, input_variables=["context", "question"]
+)
+chain_type_kwargs = {"prompt": PROMPT}
+
+chain_type_kwargs = {"prompt": PROMPT}
+
+llm = ChatGroq(
+    temperature=0.8, groq_api_key=GROQ_API_KEY, model_name="llama-3.3-70b-versatile"
+)
+
+
+qa = RetrievalQA.from_chain_type(
+    llm=llm,
+    chain_type="stuff",
+    retriever=docsearch.as_retriever(search_kwargs={"k": 2}),
+    return_source_documents=True,
+    chain_type_kwargs=chain_type_kwargs,
+)
+
+
+@app.route("/")
+def index():
+    return render_template("chat.html")
+
+
+@app.route("/get", methods=["GET", "POST"])
+def chat():
+    msg = request.form["msg"]
+    input = msg
+    print(input)
+    result = qa({"query": input})
+    print("Response : ", result["result"])
+    return str(result["result"])
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000, debug=True)
